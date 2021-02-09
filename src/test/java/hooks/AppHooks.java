35faf051e33f29f7ca1475da3e66298965f6b7e0
murat <muratj.tm@gmail.com>
2021-02-09 12:52:46 -0500

    take a screenshot, attach to the report and save to a file

--- src/test/java/hooks/AppHooks.java
+++ src/test/java/hooks/AppHooks.java
@@ -2,11 +2,16 @@ package hooks;
 
 import factory.WebDriverFactory;
 import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import utils.PropReader;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class AppHooks {
@@ -24,7 +29,21 @@ public class AppHooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            String screenshotName = scenario.getName().replaceAll(" ", "_");
+            byte[] sourcePath = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            // attach to report
+            scenario.attach(sourcePath, "image/jpg", screenshotName);
+            // to save on the file
+            try {
+                FileUtils.writeByteArrayToFile(
+                        new File("src/test/resources/screenshots/"+screenshotName+".jpg"), sourcePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         driver.quit();
     }
 }
